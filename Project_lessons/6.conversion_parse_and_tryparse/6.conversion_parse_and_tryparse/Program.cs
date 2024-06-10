2c47e38e23784903bd64045fa7d5657aef409a22
Рябов Сергей Дмитриевич <ryabov-sd@ranepa.ru>
2024-06-10 14:12:28 +0300

    lessons 6-7

---
+++ Project_lessons/6.conversion_parse_and_tryparse/6.conversion_parse_and_tryparse/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+/*
+ * конвертация строки
+ * parse и tryparse
+*/
+
+namespace _6.conversion_parse_and_tryparse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string str = "5,9";
+
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+            };
+
+            double a = double.Parse(str);
+            */
+
+            /*
+            string str = "5dawfawf";
+
+            try
+            {
+                //double d = Convert.ToDouble(str);
+                int a = int.Parse(str);
+                Console.WriteLine("Усмешная конвертация");
+            }
+            catch  (Exception)
+            {
+                Console.WriteLine("Ошибка при конвертации");
+            }
+            */
+
+            string str = "6323 dawgaws";
+
+            int a;
+
+            bool result = int.TryParse(str, out a);
+
+            if (result)
+            {
+                Console.WriteLine("Операция успешна, значение = " + a);
+            }
+            else
+            {
+                Console.WriteLine("Не удалось конвертировать!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Project_lessons/7.arithmetic_operations/7.arithmetic_operations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/*
+ * ОПЕРАТОРЫ
+ * 
+ * Арифметические операции с числами
+ * 
+ * Операнд
+ * делится на:
+ * - унарные
+ * - бинарные
+ * - тернарные
+*/
+
+namespace _7.arithmetic_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 3;
+            int c = 5;
+
+            //int result = a + b;
+            //int result = a - b;
+            //int result = a * b;
+            //double result = (double)a / b; // приведение типов
+
+            //int result = a % b; // остаток по модулю
+
+            int result = a + b * c;
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
